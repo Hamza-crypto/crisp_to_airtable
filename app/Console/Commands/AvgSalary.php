d736112a5413c98dc518ae579217bd546a265379
Hamza <6793siddique@gmail.com>
2024-06-23 20:33:28 +0500

    Removed duplicate notification

--- app/Console/Commands/AvgSalary.php
+++ app/Console/Commands/AvgSalary.php
@@ -29,12 +29,13 @@ class AvgSalary extends Command
         $currentDaysInMonth = Carbon::now()->day; // Number of days from the start of the month to the current date
         $currentAverageIncomePerDay = (int) floor($currentTotalIncome / $currentDaysInMonth / 10) * 10;
 
-        $data_array = sprintf("Current Month: Average Income Per Day - %s", $currentAverageIncomePerDay);
-        Notification::route(TelegramChannel::class, '')->notify(new AvgSalaryNotification($data_array));
+        $data_array = sprintf('Current Month: Average Income Per Day - %s', $currentAverageIncomePerDay);
+
+        //Notification::route(TelegramChannel::class, '')->notify(new AvgSalaryNotification($data_array));
 
 
         // Calculate total income and average income per day for the last 3 months
-        for ($i = 0; $i < 1; $i++) {
+        for ($i = 0; $i < 2; $i++) {
             $month = Carbon::now()->subMonths($i);
             $totalIncome = Salary::whereMonth('created_at', $month->month)
                 ->whereYear('created_at', $month->year)
@@ -43,7 +44,7 @@ class AvgSalary extends Command
             $daysInMonth = $month->daysInMonth;
             $averageIncomePerDay = (int) floor($totalIncome / $daysInMonth / 10) * 10;
 
-            $data_array = sprintf("%s: Average Income Per Day - %s", $month->format('M Y'), $averageIncomePerDay);
+            $data_array .= sprintf("\n%s: Average Income Per Day - %s", $month->format('M Y'), $averageIncomePerDay);
             Notification::route(TelegramChannel::class, '')->notify(new AvgSalaryNotification($data_array));
         }
 
